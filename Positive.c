8c0351aee1212352a3d842f96319c76da7ce75c5
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-02-23 11:08:38 +0530

    Create Positive.c

---
+++ Positive.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    clrscr();
+    if(n>0)
+    {
+        printf("It is positive");
+    }
+    if(n<0)
+    {
+        printf("It is negative");
+    }
+    if(n=0)
+    {
+        printf("it is zero");
+    }
+        getch();
+}
+
