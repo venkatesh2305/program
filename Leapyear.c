32263fc3d0996757f41e1b2e337039778af1c307
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-02-26 08:16:36 +0530

    Create Leapyear.c

---
+++ Leapyear.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+    {
+        int a;
+        scanf("%d",&a);
+        if( a%4=0)
+        printf("Leap year");
+        else
+        printf("not leap year");
+        getch();
+    }   
