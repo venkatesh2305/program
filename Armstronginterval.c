8aab7fc5ffa4ebe795a89bcf34232ee83de11a55
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-01 08:11:03 +0530

    Create Armstronginterval.c

---
+++ Armstronginterval.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main
+{
+int a,b,temp,sum=0,r,a,i;
+scanf("%d",&n1);
+scanf("%d",&n2);
+for(i=a;i<=b;i++)
+{
+temp=i;
+while(i!=0)
+{
+  a=i%10;
+  r=a*a*a;
+  sum=sum+r;
+  i=i/10;
+}
+if(sum==temp)
+{
+   printf("%d",temp);
+}
+}
+return 0;
+}
