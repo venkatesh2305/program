37705245ea0976cdc263b77abd40d112c7be0e21
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-01 08:29:47 +0530

    Create 5divisible.c

---
+++ 5divisible.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+#include<conio.h>
+void main()
+{
+    int a,i,sum=0;
+    scanf("%d",&a);
+    for(i=1;i<=5;i++)
+    {
+    sum=a*i; 
+    printf("%d\n",sum);
+    }
+    getch();
+}    
+        
