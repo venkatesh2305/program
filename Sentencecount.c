0bb9fb1c77d9d88db3562c60ba53c552b076acf4
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-01 18:48:37 +0530

    Create Sentencecount.c

---
+++ Sentencecount.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<string.h>
+void main()
+{
+    char a[100];
+    scanf("%s",&a);
+    int m,i,count=0;
+    m=strlen(a);
+    for(i=0;i<m;i++)
+    {
+        if(a[i]!=' ')
+        count++;
+    }
+    printf("%d",m); 
+    getch();
+}
