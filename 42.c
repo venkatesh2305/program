415f60f27d2c2630b980b194db161a44008a652d
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-05 12:04:44 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[10],b[13];
+    int i,n;
+    printf("enter the two sting");
+    scanf("%s,&a,&b");
+    if(a>b)
+    {
+        printf("%s",a);
+    }
+    else
+    {
+        printf("%s",b);
+    }
+getch();
+}
