2b740f2fb1a2d59f8c2b236f6b2185f52f2faf43
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-09 18:58:08 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,count=0;
+    scanf("%d",&a);
+    while(a!=0)
+    {
+    a=a/10;
+    count++;
+    }
+    printf("%d",count);
+    return 0;
+}
