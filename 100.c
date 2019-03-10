80d438b52e81a864287486c7dbac48a20e47d437
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-10 21:56:50 +0530

    Update 100.c

--- 100.c
+++ 100.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+void main()
+{
+    int n,r=1,sum=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        sum=n%10;
+        r=r*sum;
+        n=n/10;
+    }
+    printf("%d",r);
+}
