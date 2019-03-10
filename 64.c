5425a31513151fba2abb08730c53d8276cdbbc5c
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-10 07:53:29 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,m,a;
+    scanf("%d%d",&n,&m);
+    a=n+m;
+    if(a%2==0)
+    printf("even");
+    else
+    printf("odd");
+    return 0;
+}
