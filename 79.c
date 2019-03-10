2269e2dabad83343898f1ae970a1af93d810280a
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-10 17:19:03 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+	int n,m,o,i,p,flag=0;
+	scanf("%d %d",&n,&m);
+	o=m*n;
+	for(i=1;i<=o/2;i++)
+	{
+		p=i*i;
+		if(p==o)
+		{
+			printf("yes");
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+	printf("no");
+	return 0;
+}
