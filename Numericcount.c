2696a414283f29496100129d7080b2e3878da830
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-10 18:50:49 +0530

    Create Numericcount.c

---
+++ Numericcount.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char a[100];
+    scanf("%[^\t\n]s",&a);
+    int m,i,count=0,digit=0,alphs=0,space=0;
+    m=strlen(a);
+    for(i=0;i<m;i++)
+     {
+        if(a[i]>='0' && a[i]<='9')
+        digit++;  
+        else if(a[i]>='a' && a[i]<='z' || a[i]>='A' &&a[i]<='Z')
+        alphs++;
+        else
+        count++;
+    }
+    printf("%d",digit);
+    getch();
+}
